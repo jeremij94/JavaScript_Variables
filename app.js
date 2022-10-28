2cea3b792fd5a6cc37c9ffcce6c64ce04832ab90
jeremij94 <jeremij94@gmail.com>
2022-10-28 13:51:31 -0500

    Completed Exercises

--- app.js
+++ app.js
@@ -5,6 +5,37 @@ console.log(
 
 // Exercise 1
 
+const firstName = undefined;
+let lastName = 'Jackson';
+var age = 28;
+
 // Exercise 2
 
+firstName = 'Jeremi';
+lastName = 'Jackson';
+age = 28;
+
 // Exercise 3
+let language = 'JavaScript';
+let createdYear = 1995;
+let isCaseSensitive = 'true';
+
+//Exercise 4
+
+let price = 19.99;
+let isOnSale = 'false';
+let salePercentage = 15;
+let stock = 0;
+let inStock = 'false';
+let selectedSize = 'M';
+
+//Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = 'true';
+
+
+
